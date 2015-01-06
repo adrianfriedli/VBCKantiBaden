b3dc37311c4b9a9e34c207e3965edae3652e39fa
Adrian Friedli <adrianfriedli@student.ethz.ch>
2015-01-06 21:31:44 +0100

    make bash script to generate JNI classes with swig

--- android/app/src/main/jni/generateJNI.sh
+++ android/app/src/main/jni/generateJNI.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
+
 #models:
 swig3.0 -c++ -java -package ch.vbckantibaden.android.core -outdir ../java/ch/vbckantibaden/android/core -o swig/models/models_wrap.cpp swig/models/models.i
 
 #notification:
-swig3.0 -c++ -java -package ch.vbckantibaden.android.core.notification -outdir ../java/ch/vbckantibaden/android/core/notification -o swig/notification/notification_wrap.cpp swig/notification/notification.i
+#swig3.0 -c++ -java -package ch.vbckantibaden.android.core.notification -outdir ../java/ch/vbckantibaden/android/core/notification -o swig/notification/notification_wrap.cpp swig/notification/notification.i
 
 #core:
 swig3.0 -c++ -java -package ch.vbckantibaden.android.core -outdir ../java/ch/vbckantibaden/android/core -o swig/core_wrap.cpp swig/core.i
